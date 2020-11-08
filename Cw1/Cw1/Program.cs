64a83769281f43e32146014be8891183758418ed
Piotr Debski <s16213@pjwstk.edu.pl>
2020-11-08 17:44:24 +0100

    dodanie nowych wymagań

--- Cw1/Cw1/Program.cs
+++ Cw1/Cw1/Program.cs
@@ -2,7 +2,8 @@
 using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using System.Text.RegularExpressions;
+using System.Net;
+using System.Collections.Generic;
 
 namespace Cw1
 {
@@ -10,23 +11,59 @@ namespace Cw1
     {
         public static async Task Main(string[] args)
         {
-            var httpClient = new HttpClient();
-            var response = await httpClient.GetAsync("https://pja.edu.pl");
+           
+            using var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(args[0]);
 
-            if (response.IsSuccessStatusCode)
+          
+
+            if (args.Length == 0)
             {
+                throw new ArgumentNullException("You have to pass the URL as first parameter");
+            }
+
+            bool result = Uri.TryCreate(args[0], UriKind.Absolute, out Uri uriResult)
+                && (uriResult.Scheme == Uri.UriSchemeHttps || uriResult.Scheme == Uri.UriSchemeHttp);
+
+            if (!result)
+            {
+                throw new ArgumentException("URL nie jest poprawny");
+            }
+
+            Console.WriteLine(uriResult);
+
+           if (response.StatusCode == HttpStatusCode.OK)
+           {
                 var html = await response.Content.ReadAsStringAsync();
                 var regex = new Regex("[a-zA-Z0-9]+@[a-z.]+");
 
                 MatchCollection matches = regex.Matches(html);
 
-                foreach(var i in matches)
-                {
-                    Console.WriteLine(i);
-                }
-            }
+                HashSet<string> uniqEmails = new HashSet<string>();
 
-            Console.WriteLine("Koniec!");
+                if(matches == null)
+                {
+                    Console.WriteLine("Brak emaili do wyświetlenia");
+                }
+                else
+                {
+                    foreach (var i in matches)
+                    {
+                        uniqEmails.Add(i.ToString());
+                    }
+                    
+                    foreach (var i in uniqEmails)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }                
+            }
+           
+            else
+            {
+                Console.WriteLine("Błąd w czasie pobierania strony");
+            }
+            httpClient.Dispose();
         }
     }
 }
